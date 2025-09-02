b86324d24ebcfd27da460138755ff74afba5fc9a
animeshmaiti <a.maiti770@gmail.com>
2025-09-02 23:01:48 +0530

    init inner collection1-2

---
+++ src/pages/collection/Collection.jsx
@@ -0,0 +1,44 @@
+import collection1 from '@assets/collection1.jpg';
+import collection2 from '@assets/collection2.jpg';
+import { useRevealOnRoute } from '../../hook/useRevealOnRoute';
+import { Link } from 'react-router-dom';
+import './collection.css';
+
+const Collection = () => {
+  useRevealOnRoute((sr) => {
+    sr.reveal('.collection__explore:nth-child(1)', { origin: 'right' });
+    sr.reveal('.collection__explore:nth-child(2)', { origin: 'left' });
+  });
+  return (
+    <section class="collection section" id="collection">
+      <div className="collection__container container">
+        <div className="collection__explore">
+          <div className="collection__data-men">
+            <h2 className="collection__title">
+              Curly hair <br /> Collection
+            </h2>
+            <Link to="/collection1" className="button__link">
+              Explore
+              <i className="ri-arrow-right-line"></i>
+            </Link>
+          </div>
+          <img src={collection1} alt="" />
+        </div>
+        <div className="collection__explore">
+          <img src={collection2} alt="" />
+          <div className="collection__data-women">
+            <h2 className="collection__title">
+              Sort hair <br /> Collection
+            </h2>
+            <Link to="/collection2" className="button__link">
+              Explore
+              <i className="ri-arrow-right-line"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Collection;
---
+++ src/pages/collections/Collection1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Collection1 = () => {
+  return (
+    <div>Collection1</div>
+  )
+}
+
+export default Collection1
